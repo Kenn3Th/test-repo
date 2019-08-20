61f288229df19bbc640530b98aa4d39b24666ba3
Kenneth Ramos Eikrehagen <Awesome@Kenneths-MacBook-Pro-1672.local>
2019-08-20 20:27:25 +0200

    Initial commit

---
+++ VScode_test.py
@@ -0,0 +1,25 @@
+import sys, requests
+
+# print(sys.version)    #Printer hvilken versjon som blir brukt
+print(sys.executable)  # Printer hvor filen befinner seg
+
+
+def Hilsen(hvem):
+    Hils = "Hei, {}".format(hvem)
+    return Hils
+
+
+# print(Hilsen("Kenneth"))
+# print(Hilsen("resten av verden"))
+
+r = requests.get("https://snl.no")
+print(r.status_code)
+
+"""
+Code-runner er en ekstention som kun har "read only - output"
+For å kunne bruke input må man kjøre koden i terminalen. 
+Enten på gamle måten eller høyre klikk i vinduet å kjør i terminalen.
+"""
+name = input("Your name? ")
+print(Hilsen(name))
+
